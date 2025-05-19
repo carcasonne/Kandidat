ab0df6253275d50320195b371bb423366e196f8a
carcasonne <villumsonne@tutanota.com>
2025-05-19 19:33:12 +0200

    fix paths

--- AST/EER_benchmark.py
+++ AST/EER_benchmark.py
@@ -27,12 +27,10 @@ import inspect
 
 # === CONFIG ===
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-# AST_MODEL_CHECKPOINT = "checkpoints/asvspoof-ast-model15_100K_20250506_054106"  # Replace with your actual saved path
-AST_MODEL_CHECKPOINT = "checkpoints/asvspoof-ast-model0_20250514_133135"
-PRETRAIN_MODEL_CHECKPOINT = "checkpoints/asvspoof-pretrain-model19_20250507_081555"
-ADD_DATASET_PATH = "spectrograms/ADD"  # Replace with your actual ADD dataset root
-FOR_DATASET_PATH = "spectrograms/FoR/for-2sec/for-2seconds"
-ASVS_DATASET_PATH = "spectrograms"
+AST_MODEL_CHECKPOINT = "/home/alsk/Kandidat/AST/checkpoints/asvspoof-ast-model15_100K_20250506_054106"
+ADD_DATASET_PATH = "/home/alsk/Kandidat/AST/spectrograms/ADD"  # Replace with your actual ADD dataset root
+FOR_DATASET_PATH = "/home/alsk/Kandidat/AST/spectrograms/FoR/for-2sec/for-2seconds"
+ASVS_DATASET_PATH = "/home/alsk/Kandidat/AST/spectrograms"
 BATCH_SIZE = 16
 
 def load_modified_ast_model(base_model_name, finetuned_model_path, device=None):
